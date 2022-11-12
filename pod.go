5ab330d9236809846257f7dcffa38c07e00fd8b2
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2022-11-12 12:09:45 +0300

    Validation

--- pod.go
+++ pod.go
@@ -3,9 +3,9 @@ package tau
 import "github.com/BurntSushi/toml"
 
 type Pod struct {
-	Kind       string      `validate:"eq=pod"`
+	Kind       string      `validate:"required,eq=pod"`
 	Name       string      `validate:"required"`
-	Containers []Container `validate:"required,dive,required"`
+	Containers []Container `validate:"required,dive"`
 }
 
 func NewPod(data []byte) (pod Pod, err error) {
